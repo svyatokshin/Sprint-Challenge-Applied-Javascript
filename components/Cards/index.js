10be87b04eb23d492c22beaf022394ce676082fe
svyatoslav-okshin <svyatokshin@gmail.com>
2020-01-17 13:11:27 -0500

    finished Cards section. MET MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,52 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const cardsContainer = document.querySelector('.cards-container');
+
+function cardCreator(obj) {
+    // Create Elements
+    let card = document.createElement('div');
+    let cardHeadline = document.createElement('div');
+    let cardAuthorContainer = document.createElement('div')
+    let cardImgContainer = document.createElement('div');
+    let cardImg = document.createElement('img');
+    let cardAuthor = document.createElement('span');
+
+    // Attach classes to respective elements created
+    card.classList.add('card');
+    cardHeadline.classList.add('headline');
+    cardAuthorContainer.classList.add('author');
+    cardImgContainer.classList.add('img-container');
+
+    // Append everything to main card so that it can be exported to HTML
+    card.appendChild(cardHeadline);
+    card.appendChild(cardAuthorContainer);
+    cardAuthorContainer.appendChild(cardImgContainer);
+    cardAuthorContainer.appendChild(cardAuthor);
+    cardImgContainer.appendChild(cardImg);
+
+    // Adding content to all the elements
+    cardHeadline.textContent = obj.headline;
+    cardImg.src = obj.authorPhoto;
+    cardAuthor.textContent = `By ${obj.authorName}`;
+
+    return card;
+}
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(response => {
+    // console.log("Object Array: ", response.data);
+    let articles = response.data.articles;
+    console.log(articles);
+    for (topic in articles) {
+        articles[topic].forEach(info => {
+            cardsContainer.appendChild(cardCreator(info));
+            console.log(articles[topic]);
+        });
+    }
+})
+.catch(error => {
+    console.log("Data was not returned: ", error);
+})
